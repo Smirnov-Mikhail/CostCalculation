5f012dad023d42dd375260690cc5e5552291f02f
Smirnov Mikhail <sm_m_a@mail.ru>
2015-02-07 23:34:15 +0300

    Расчёт стоимости основания, лаг и стен.

---
+++ Cost calculation/Cost calculation/CostForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Cost_calculation
+{
+    partial class CostForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lengthtTextBox = new System.Windows.Forms.TextBox();
+            this.widthTextBox = new System.Windows.Forms.TextBox();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.NameMaterial = new System.Windows.Forms.Label();
+            this.Next = new System.Windows.Forms.Button();
+            this.OutputData = new System.Windows.Forms.Label();
+            this.memoLengthLabel = new System.Windows.Forms.Label();
+            this.memoWidthLabel = new System.Windows.Forms.Label();
+            this.memoHeightLabel = new System.Windows.Forms.Label();
+            this.CurrentTaskLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lengthtTextBox
+            // 
+            this.lengthtTextBox.Location = new System.Drawing.Point(92, 66);
+            this.lengthtTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lengthtTextBox.Name = "lengthtTextBox";
+            this.lengthtTextBox.Size = new System.Drawing.Size(67, 23);
+            this.lengthtTextBox.TabIndex = 0;
+            // 
+            // widthTextBox
+            // 
+            this.widthTextBox.Location = new System.Drawing.Point(168, 66);
+            this.widthTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.widthTextBox.Name = "widthTextBox";
+            this.widthTextBox.Size = new System.Drawing.Size(67, 23);
+            this.widthTextBox.TabIndex = 1;
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(244, 66);
+            this.heightTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(67, 23);
+            this.heightTextBox.TabIndex = 2;
+            // 
+            // NameMaterial
+            // 
+            this.NameMaterial.AutoSize = true;
+            this.NameMaterial.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameMaterial.Location = new System.Drawing.Point(36, 66);
+            this.NameMaterial.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.NameMaterial.Name = "NameMaterial";
+            this.NameMaterial.Size = new System.Drawing.Size(36, 17);
+            this.NameMaterial.TabIndex = 3;
+            this.NameMaterial.Text = "Дом";
+            // 
+            // Next
+            // 
+            this.Next.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Next.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Next.Location = new System.Drawing.Point(369, 129);
+            this.Next.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(115, 41);
+            this.Next.TabIndex = 4;
+            this.Next.Text = "Далее";
+            this.Next.UseVisualStyleBackColor = true;
+            this.Next.Click += new System.EventHandler(this.Next_Click);
+            // 
+            // OutputData
+            // 
+            this.OutputData.AutoSize = true;
+            this.OutputData.Location = new System.Drawing.Point(35, 169);
+            this.OutputData.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OutputData.Name = "OutputData";
+            this.OutputData.Size = new System.Drawing.Size(0, 17);
+            this.OutputData.TabIndex = 5;
+            // 
+            // memoLengthLabel
+            // 
+            this.memoLengthLabel.AutoSize = true;
+            this.memoLengthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.memoLengthLabel.Location = new System.Drawing.Point(92, 43);
+            this.memoLengthLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.memoLengthLabel.Name = "memoLengthLabel";
+            this.memoLengthLabel.Size = new System.Drawing.Size(48, 17);
+            this.memoLengthLabel.TabIndex = 6;
+            this.memoLengthLabel.Text = "длина";
+            // 
+            // memoWidthLabel
+            // 
+            this.memoWidthLabel.AutoSize = true;
+            this.memoWidthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.memoWidthLabel.Location = new System.Drawing.Point(164, 43);
+            this.memoWidthLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.memoWidthLabel.Name = "memoWidthLabel";
+            this.memoWidthLabel.Size = new System.Drawing.Size(59, 17);
+            this.memoWidthLabel.TabIndex = 7;
+            this.memoWidthLabel.Text = "ширина";
+            // 
+            // memoHeightLabel
+            // 
+            this.memoHeightLabel.AutoSize = true;
+            this.memoHeightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.memoHeightLabel.Location = new System.Drawing.Point(240, 43);
+            this.memoHeightLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.memoHeightLabel.Name = "memoHeightLabel";
+            this.memoHeightLabel.Size = new System.Drawing.Size(55, 17);
+            this.memoHeightLabel.TabIndex = 8;
+            this.memoHeightLabel.Text = "высота";
+            // 
+            // CurrentTaskLabel
+            // 
+            this.CurrentTaskLabel.AutoSize = true;
+            this.CurrentTaskLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CurrentTaskLabel.Location = new System.Drawing.Point(16, 11);
+            this.CurrentTaskLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CurrentTaskLabel.Name = "CurrentTaskLabel";
+            this.CurrentTaskLabel.Size = new System.Drawing.Size(213, 20);
+            this.CurrentTaskLabel.TabIndex = 9;
+            this.CurrentTaskLabel.Text = "Введите параметры дома.";
+            // 
+            // CostForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 376);
+            this.Controls.Add(this.CurrentTaskLabel);
+            this.Controls.Add(this.memoHeightLabel);
+            this.Controls.Add(this.memoWidthLabel);
+            this.Controls.Add(this.memoLengthLabel);
+            this.Controls.Add(this.OutputData);
+            this.Controls.Add(this.Next);
+            this.Controls.Add(this.NameMaterial);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.widthTextBox);
+            this.Controls.Add(this.lengthtTextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "CostForm";
+            this.Text = "Стоимость здания";
+            this.Load += new System.EventHandler(this.CostForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.TextBox lengthtTextBox;
+        private System.Windows.Forms.TextBox widthTextBox;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.Label NameMaterial;
+        private System.Windows.Forms.Button Next;
+        private System.Windows.Forms.Label OutputData;
+        private System.Windows.Forms.Label memoLengthLabel;
+        private System.Windows.Forms.Label memoWidthLabel;
+        private System.Windows.Forms.Label memoHeightLabel;
+        private System.Windows.Forms.Label CurrentTaskLabel;
+    }
+}
+
---
+++ Cost calculation/Cost calculation/CostForm.cs
@@ -0,0 +1,271 @@
+﻿namespace Cost_calculation
+{
+    using System;
+    using System.Windows.Forms;
+
+    public partial class CostForm : Form
+    {
+        public CostForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CostForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Действия при нажатии на клавишу "Далее".
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Next_Click(object sender, EventArgs e)
+        {
+            if (this.CurrentTaskLabel.Text == "Введите параметры дома.")
+            {
+                HandlerHouseData();
+            }
+            else if (this.CurrentTaskLabel.Text == "Введите параметры бруса для основания.")
+            {
+                HandlerBaseData();
+            }
+            else if (this.CurrentTaskLabel.Text == "Выберите стену, к которой привяжем лаги.")
+            {
+                HandlerChoiceWallForLagsData();
+            }
+            else if (this.CurrentTaskLabel.Text == "Введите параметры досок и бруса для лаг.")
+            {
+                HandlerLagsData();
+            }
+            else if (this.CurrentTaskLabel.Text == "Введите параметры бруса для стен.")
+            {
+                HandlerWallsData();
+            }
+        }
+
+        /// <summary>
+        /// Обрабатываем входные данные дома.
+        /// </summary>
+        private void HandlerHouseData()
+        {
+            house.length = Convert.ToDouble(this.lengthtTextBox.Text);
+            house.width = Convert.ToDouble(this.widthTextBox.Text);
+            house.height = Convert.ToDouble(this.heightTextBox.Text);
+
+            this.NameMaterial.Text = "Брус";
+            this.OutputData.Text = "Рассматриваем дом: " + house.length + "х" + house.width + "х" + house.height;
+            ClearLabels();
+            this.CurrentTaskLabel.Text = "Введите параметры бруса для основания.";
+            this.lengthtTextBox.Text = "6";
+        }
+
+        /// <summary>
+        /// Обрабатываем входные данные бруса для основания.
+        /// </summary>
+        private void HandlerBaseData()
+        {
+            double numberTimber = house.Perimeter() / Convert.ToDouble(this.lengthtTextBox.Text);
+
+            this.OutputData.Text = "Нужно бруса для основания: " + numberTimber.ToString();
+            ClearLabels();
+            this.CurrentTaskLabel.Text = "Выберите стену, к которой привяжем лаги.";
+            this.lengthtTextBox.Text = "6";
+
+            this.Controls.Remove(lengthtTextBox);
+            this.Controls.Remove(widthTextBox);
+            this.Controls.Remove(heightTextBox);
+            this.Controls.Remove(NameMaterial);
+            this.Controls.Remove(memoLengthLabel);
+            this.Controls.Remove(memoWidthLabel);
+            this.Controls.Remove(memoHeightLabel);
+
+            LengthWall = new Button();
+            LengthWall.AutoSize = true;
+            LengthWall.Location = new System.Drawing.Point(12, 40);
+            LengthWall.Name = "LengthWallButton";
+            LengthWall.Text = "Стена длиной " + house.length.ToString();
+            LengthWall.UseVisualStyleBackColor = true;
+            LengthWall.Click += new System.EventHandler(this.LengthWall_Click);
+            this.Controls.Add(LengthWall);
+
+            WidthWall = new Button();
+            WidthWall.AutoSize = true;
+            WidthWall.Location = new System.Drawing.Point(180, 40);
+            WidthWall.Name = "LengthWallButton";
+            WidthWall.Text = "Стена длиной " + house.width.ToString();
+            WidthWall.UseVisualStyleBackColor = true;
+            WidthWall.Click += new System.EventHandler(this.WidthWall_Click);
+            this.Controls.Add(WidthWall);
+        }
+
+        private void LengthWall_Click(object sender, EventArgs e)
+        {
+            house.wallForLags = true;
+            this.Controls.Remove(LengthWall);
+            this.Controls.Remove(WidthWall);
+        }
+
+        private void WidthWall_Click(object sender, EventArgs e)
+        {
+            house.wallForLags = false;
+            this.Controls.Remove(LengthWall);
+            this.Controls.Remove(WidthWall);
+        }
+
+        /// <summary>
+        /// Обрабатываем входные данные бруса для основания и черновые доски.
+        /// </summary>
+        private void HandlerChoiceWallForLagsData()
+        {
+            this.Controls.Add(lengthtTextBox);
+            this.Controls.Add(widthTextBox);
+            this.Controls.Add(heightTextBox);
+            this.Controls.Add(NameMaterial);
+            this.Controls.Add(memoLengthLabel);
+            this.Controls.Add(memoWidthLabel);
+            this.Controls.Add(memoHeightLabel);
+
+            if (house.wallForLags)
+                this.OutputData.Text = "Привязали лаги к стене с длиной " + house.length.ToString();
+            else
+                this.OutputData.Text = "Привязали лаги к стене с длиной " + house.width.ToString();
+            ClearLabels();
+            this.CurrentTaskLabel.Text = "Введите параметры досок и бруса для лаг.";
+            this.lengthtTextBox.Text = "6";
+
+            TempLabel = new Label();
+            TempLabel.AutoSize = true;
+            TempLabel.Location = new System.Drawing.Point(36, 100);
+            TempLabel.Name = "TempLabel";
+            TempLabel.Text = "Доска";
+            this.Controls.Add(TempLabel);
+
+            TempTextBox1 = new TextBox();
+            TempTextBox1.Location = new System.Drawing.Point(92, 100);
+            TempTextBox1.Name = "TempTextBox1";
+            TempTextBox1.Size = new System.Drawing.Size(67, 23);
+            TempTextBox1.Text = "6";
+            this.Controls.Add(TempTextBox1);
+
+            TempTextBox2 = new TextBox();
+            TempTextBox2.Location = new System.Drawing.Point(168, 100);
+            TempTextBox2.Name = "TempTextBox1";
+            TempTextBox2.Size = new System.Drawing.Size(67, 23);
+            this.Controls.Add(TempTextBox2);
+
+            TempTextBox3 = new TextBox();
+            TempTextBox3.Location = new System.Drawing.Point(244, 100);
+            TempTextBox3.Name = "TempTextBox1";
+            TempTextBox3.Size = new System.Drawing.Size(67, 23);
+            TempTextBox3.Text = "0,02";
+            this.Controls.Add(TempTextBox3);
+        }
+
+        /// <summary>
+        /// Обрабатываем входные данные бруса для лагов.
+        /// </summary>
+        private void HandlerLagsData()
+        {
+            house.distanceBetweenLags = 0.8;
+
+            double numberTimber;
+            double numberTimberOther = 1;
+
+            if (house.wallForLags)
+            {
+                numberTimber = house.length / house.distanceBetweenLags - 2;
+                if (house.width > 6)
+                    numberTimberOther = 2;
+            }
+            else
+            {
+                numberTimber = house.width / house.distanceBetweenLags - 2;
+                if (house.length > 6)
+                    numberTimberOther = 2;
+            }
+
+            this.OutputData.Text = "Нужно бруса для лагов: " + (numberTimber + numberTimberOther).ToString();
+
+            double numberBoard;
+            double areaBoard = Convert.ToDouble(this.TempTextBox1.Text) * Convert.ToDouble(this.TempTextBox2.Text);
+            if (house.wallForLags)
+            {
+                numberBoard = ((house.length - numberTimber * Convert.ToDouble(this.widthTextBox.Text)) * house.width) 
+                    / areaBoard;
+            }
+            else
+            {
+                numberBoard = ((house.width - numberTimber * Convert.ToDouble(this.widthTextBox.Text)) * house.length)
+                    / areaBoard;
+            }
+
+            this.OutputData.Text += "\nНужно досок:" + numberBoard.ToString();
+            ClearLabels();
+            this.CurrentTaskLabel.Text = "Введите параметры бруса для стен.";
+            this.lengthtTextBox.Text = "6";
+
+            removeTempElements();
+        }
+
+        /// <summary>
+        /// Обрабатываем входные данные бруса для стен.
+        /// </summary>
+        private void HandlerWallsData()
+        {
+            double numberLine = house.height / Convert.ToDouble(this.heightTextBox.Text);
+            double numberTimber = house.Perimeter() / Convert.ToDouble(this.lengthtTextBox.Text) * numberLine;
+
+            this.NameMaterial.Text = "";
+            this.OutputData.Text = "Нужно бруса: " + numberTimber.ToString();
+            ClearLabels();
+        }
+
+        /// <summary>
+        /// Очищает поля ввода данных.
+        /// </summary>
+        private void ClearLabels()
+        {
+            this.lengthtTextBox.Text = "";
+            this.widthTextBox.Text = "";
+            this.heightTextBox.Text = "";
+        }
+
+        /// <summary>
+        /// Удаляем с формы временные элементы.
+        /// </summary>
+        private void removeTempElements()
+        {
+            this.Controls.Remove(TempLabel);
+            this.Controls.Remove(TempTextBox1);
+            this.Controls.Remove(TempTextBox2);
+            this.Controls.Remove(TempTextBox3);
+        }
+
+        /// <summary>
+        /// Структура дома.
+        /// </summary>
+        private struct House
+        {
+            public double length;
+            public double width;
+            public double height;
+
+            public bool wallForLags;
+            public double distanceBetweenLags;
+
+            public double Perimeter()
+            {
+                return 2 * (length + width);
+            }
+        }
+
+        private House house;
+        private Button LengthWall;
+        private Button WidthWall;
+        private Label TempLabel;
+        private TextBox TempTextBox1;
+        private TextBox TempTextBox2;
+        private TextBox TempTextBox3;
+    }
+}
